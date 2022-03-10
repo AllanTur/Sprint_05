2afdccf10b25f0b16e87a7796bb283dd9d8eb17a
allan turpin <allan974.turpin@gmail.com>
2022-03-10 17:59:38 +0100

    Finition

--- rest-countries/index.js
+++ rest-countries/index.js
@@ -11,7 +11,11 @@ async function render() {
   } catch (e) {
     console.error(e);
     console.log("Dans le catch");
-
+    // let btn = document.createElementById("refresh");
+    // let txt = document.createTextNode("boutton raffraichir");
+    // btn.appendChild(txt);
+    // document.getElementById("refresh").textContent = "";
+    // document.getElementById("refresh").appendChild(btn);
     // btn.addEventListener("click", function () {
     //   render();
     // });
@@ -19,19 +23,18 @@ async function render() {
 }
 
 render();
-document.getElementById("refresh").style.color = "blue";
-// document.getElementById("refresh").style.background = "red";
+document.getElementById("refresh").style.color = "grey";
 document.getElementById("refresh").style.border = "none";
 document.getElementById("refresh").style.borderRadius = "5px";
 document.getElementById("refresh").style.margin = "50px";
 document.getElementById("refresh").style.height = "50px";
 document.getElementById("refresh").style.width = "200px";
 function mouseDown() {
-  document.getElementById("refresh").style.color = "red";
+  document.getElementById("refresh").style.color = "aqua";
 }
 
 function mouseUp() {
-  document.getElementById("refresh").style.color = "purple";
+  document.getElementById("refresh").style.color = "grey";
 }
 // let btn = document.createElementById("refresh");
 // let txt = document.createTextNode("boutton raffraichir");
